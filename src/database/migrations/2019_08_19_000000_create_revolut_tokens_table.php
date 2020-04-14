4331cc0973d6e3a4e2269f5e4a0bcc813149dad8
Dom <dom@Doms-MacBook-Pro.local>
2020-04-14 16:50:18 +0100

    fix schema

--- src/database/migrations/2019_08_19_000000_create_revolut_tokens_table.php
+++ src/database/migrations/2019_08_19_000000_create_revolut_tokens_table.php
@@ -14,7 +14,7 @@ class CreateRevolutTokensTable extends Migration
     public function up()
     {
         Schema::create(config('revolut.tokens_table'), function (Blueprint $table) {
-			$table->unsignedBigInteger('id');
+			$table->bigIncrements('id');
 			$table->string('type');
             $table->mediumText('value');
             $table->boolean('is_encrypted')->default(false);
